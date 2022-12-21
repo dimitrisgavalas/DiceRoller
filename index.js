e8fc13de291aa5363d2d8c67b6d9be7977395046
dimitrisgavalas <40126678+dimitrisgavalas@users.noreply.github.com>
2022-12-21 13:29:14 +0200

    Added dice roller files

---
+++ index.js
@@ -0,0 +1,30 @@
+function rollDice(){
+
+  //random Number 1
+  var randomNumber1 = Math.floor(Math.random()*6 + 1); //1-6
+  var randomDiceImage = "dice" + randomNumber1 +".png";
+  var randomImageSource = "images/" + randomDiceImage;
+
+  document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+
+
+
+//random Number 2
+  var randomNumber2 = Math.floor(Math.random()*6 + 1);
+  var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+  var randomImageSource2 = "images/" + randomDiceImage2;
+
+  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🏴󠁳󠁤󠁲󠁳󠁿 Player 1 Wins!";
+  }else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🏴󠁳󠁤󠁲󠁳󠁿";
+  }
+  else{
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+
+}
